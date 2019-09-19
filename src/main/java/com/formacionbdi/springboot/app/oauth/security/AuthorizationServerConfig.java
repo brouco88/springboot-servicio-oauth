0d881febaf99b0c219b3eee8e59fcefa6bd4510f
Brouco <brouco@optaresolutions.com>
2019-09-19 12:10:32 +0200

    Configurando Authorization

--- src/main/java/com/formacionbdi/springboot/app/oauth/security/AuthorizationServerConfig.java
+++ src/main/java/com/formacionbdi/springboot/app/oauth/security/AuthorizationServerConfig.java
@@ -10,7 +10,6 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
@@ -26,12 +25,19 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
 	@Override
 	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
-		super.configure(security);
+		security.tokenKeyAccess("permitAll")
+		.checkTokenAccess("isAutenticated()");
 	}
 
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-		super.configure(clients);
+		clients.inMemory()
+		.withClient("frotendapp")
+		.secret(passwordEncoder.encode("12345"))
+		.scopes("read","write")
+		.authorizedGrantTypes("password","refresh_token")
+		.accessTokenValiditySeconds(3600)
+		.refreshTokenValiditySeconds(3600);
 	}
 
 	@Override
